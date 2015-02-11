663a45579c6c77fdfe0da355b9259b71747327d9
Marc van der Peet <marcvanderpeet@gmail.com>
2015-02-11 22:15:37 +0000

    added sendgrid and attempt to include confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,3 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
+
+
+# NameError in Devise::RegistrationsController#new
+# undefined local variable or method `confirmed_at' for #<User:0x007f98f1413ed0>
