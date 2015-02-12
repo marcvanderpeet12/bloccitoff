f4130b6fe7f43a20878f5aee50d78725a13b78ed
Marc van der Peet <marcvanderpeet@gmail.com>
2015-02-12 00:11:37 +0000

    included dayscount function

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -3,6 +3,10 @@ class Todo < ActiveRecord::Base
 
   validates :description, length: { minimum: 5 }
 
+   def days_left
+    7 - (DateTime.now.to_date - created_at.to_date).to_i
+   end
+
   def self.search(query)
     where("description like ?", "%#{query}%") 
   end  
